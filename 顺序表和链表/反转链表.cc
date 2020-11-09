95df611a646142d288ed4d0b45427d9fac3c6e1f
lkimprove <47627543+lkimprove@users.noreply.github.com>
2020-11-09 19:38:33 +0800

    Create 反转链表.cc

---
+++ 顺序表和链表/反转链表.cc
@@ -0,0 +1,87 @@
+//迭代法
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* reverseList(ListNode* head) {
+        if(head == nullptr){
+            return head;
+        }
+
+        ListNode* prev = nullptr;
+        ListNode* curr = head;
+        while(curr != nullptr){
+            ListNode* temp = curr->next;
+            curr->next = prev;
+            prev = curr;
+            curr = temp;
+        }
+
+        return prev;
+    }
+};
+
+
+//递归
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* reverseList(ListNode* head) {
+        if(head == nullptr || head->next == nullptr){
+            return head;
+        }
+
+        ListNode* ret = reverseList(head->next);
+        head->next->next = head;
+        head->next = nullptr;
+
+        return ret;
+    }
+};
+
+
+//妖魔化的双指针
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* reverseList(ListNode* head) {
+        if(head == nullptr){
+            return head;
+        }
+
+        ListNode* curr = head;
+        while(head->next != nullptr){
+            ListNode* temp = head->next->next;
+            head->next->next = curr;
+            curr = head->next;
+            head->next = temp;
+        }
+
+        return curr;
+    }
+};
+
+
+
+
+
